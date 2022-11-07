13209efe29bf68ab66c690491f7f9374979bd548
Kai Thomas <45699223+kaiThomas236@users.noreply.github.com>
2022-11-07 15:26:20 -0700

    spacing, sizing, responsiveness
    added specific support for Firefox as the Contact section was breaking on mobile Firefox

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-// $('section-box .card-header[aria-expanded="true"]').addClass('is-open');
-// var services = document.getElementById("services");
-// console.log(services.firstChild)
+function editHtml() {
+    let contact = document.getElementById("contact");
+
+    let firstOuter = contact.getElementsByClassName("map-wrapper")[0];
+    let secondOuter = contact.getElementsByClassName("second-col")[0];
+    let firstInner = secondOuter.getElementsByClassName("icon-col")[0];
+    let secondInner = secondOuter.getElementsByClassName("contact-col")[0];
+
+    firstOuter.classList.remove("col-auto");
+    secondOuter.classList.remove("col-auto");
+    firstInner.classList.remove("col-auto");
+    secondInner.classList.remove("col-auto");
+    
+    firstOuter.classList.add("col-6");
+    secondOuter.classList.add("col-6");
+    firstInner.classList.add("col-2");
+    secondInner.classList.add("col-10");
+
+    firstInner.classList.add("firefox-size");
+    secondInner.classList.add("firefox-size");
+
+    firstInner.parentElement.classList.add("firefox-parent");
+}
+
+
+
+function checkFirefox() {
+    return(navigator.userAgent.indexOf("Firefox") > -1);
+}
+
+var isFirefox = checkFirefox();
+if (isFirefox) {
+    editHtml();
+}
